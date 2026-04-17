404978d21fe8775caa5135a9aab6cfc82db5d91f
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-17 08:16:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
+import pandas as pd
 
+def guardar_datos(df):
+    df.to_csv("alumnos.csv", index=False)
+
+def cargar_datos():
+    try:
+        return pd.read_csv("alumnos.csv")
+    except:
+        return pd.DataFrame()
 st.title("📚 Class Manager")
 
 opcion = st.selectbox("Selecciona una opción", [
@@ -8,7 +17,23 @@ opcion = st.selectbox("Selecciona una opción", [
     "Importar lista",
     "Ver alumnos"
 ])
+import os
 
+if st.button("🔄 Restablecer sistema"):
+    if os.path.exists("alumnos.csv"):
+        os.remove("alumnos.csv")
+    st.warning("Sistema reiniciado")
+import shutil
+
+def backup():
+    if os.path.exists("alumnos.csv"):
+        shutil.copy("alumnos.csv", "backup_alumnos.csv")
+        
+if st.button("♻️ Recuperar respaldo"):
+    if os.path.exists("backup_alumnos.csv"):
+        shutil.copy("backup_alumnos.csv", "alumnos.csv")
+        st.success("Datos recuperados")
+        
 if opcion == "Crear clase":
     nombre_clase = st.text_input("Nombre de la clase")
     if st.button("Crear"):

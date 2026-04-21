70691d5f45c7152555a0120cd570fd1a6cb8f47b
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-21 07:37:01 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,21 +1,24 @@
 import firebase_admin
-from firebase_admin import credentials, auth, firestore
+from firebase_admin import credentials, firestore
+import streamlit as st
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+# Evita reinicializar Firebase
+if not firebase_admin._apps:
 
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    })
+
+    firebase_admin.initialize_app(cred)
+
+# Base de datos
 db = firestore.client()
-
-def register_user(email, password):
-    try:
-        user = auth.create_user(email=email, password=password)
-        return user.uid
-    except:
-        return None
-
-def login_user(email):
-    try:
-        user = auth.get_user_by_email(email)
-        return user.uid
-    except:
-        return None
